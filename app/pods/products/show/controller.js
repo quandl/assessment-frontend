19d045516ef03c46505e3e9389f29fb524469d43
karly mraud <karly.mraud@hotmail.com>
2020-09-30 10:56:20 -0400

    remove all files

--- app/pods/products/show/controller.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Controller.extend({
-  publisher: Ember.computed.alias('model.publisher'),
-  product: Ember.computed.alias('model.product'),
-  productData: Ember.computed.alias('product.data'),
-});
